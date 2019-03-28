a3390f234cbb77c40953d01c7434a0d858d7b813
jiangsssw <1941886426@qq.com>
2019-03-28 11:32:08 +0800

    增加登录验证权限代码

---
+++ src/main/java/wj/intercept/LoginIntercept.java
@@ -0,0 +1,39 @@
+package wj.intercept;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import wj.entity.dataBaseMapping.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginIntercept implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        String uri = httpServletRequest.getRequestURI();
+        if (uri.indexOf("/login")>0){
+            return true;
+        }
+        HttpSession session = httpServletRequest.getSession();
+        User u = (User) session.getAttribute("User");
+        if (u!=null){
+            return true;
+        }
+
+        httpServletRequest.setAttribute("result","您还没有登录请先登录");
+        httpServletRequest.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(httpServletRequest,httpServletResponse);
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
